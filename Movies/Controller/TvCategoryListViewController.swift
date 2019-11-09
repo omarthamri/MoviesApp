493514edc1ef34c9c1a31cc8cb19353c6d5e9f9a
Omar Thamri <omarthamri4@gmail.com>
2019-11-09 15:18:42 +0100

    #0006 implement list TV Category

--- Movies/Controller/TvCategoryListViewController.swift
+++ Movies/Controller/TvCategoryListViewController.swift
@@ -10,6 +10,19 @@ import UIKit
 
 class TvCategoryListViewController: UIViewController {
     
+    let TvCategoryListViewCellId = "TvCategoryListViewCellId"
+    
+    lazy var tvCategoryListCV: UICollectionView = {
+        let layout = UICollectionViewFlowLayout()
+        layout.scrollDirection = .vertical
+        let tclc = UICollectionView(frame: .zero, collectionViewLayout: layout)
+        tclc.delegate = self
+        tclc.dataSource = self
+        tclc.translatesAutoresizingMaskIntoConstraints = false
+        tclc.backgroundColor = UIColor.init(white: 0.2, alpha: 1)
+        return tclc
+    }()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         setupView()
@@ -20,11 +33,42 @@ class TvCategoryListViewController: UIViewController {
         navigationController?.navigationBar.tintColor = .white
         navigationItem.title = "TV Category"
         view.backgroundColor = UIColor.init(white: 0.2, alpha: 1)
+        view.addSubview(tvCategoryListCV)
+        tvCategoryListCV.register(UICollectionViewCell.self, forCellWithReuseIdentifier: TvCategoryListViewCellId)
     }
     
     func setupConstraints() {
-        
+        view.addConstraints(NSLayoutConstraint.constraints(withVisualFormat: "H:|-10-[v0]-10-|", options: NSLayoutFormatOptions(), metrics: nil, views: ["v0":tvCategoryListCV]))
+        view.addConstraints(NSLayoutConstraint.constraints(withVisualFormat: "V:|-10-[v0]-10-|", options: NSLayoutFormatOptions(), metrics: nil, views: ["v0":tvCategoryListCV]))
     }
+    
+    
+}
+
+extension TvCategoryListViewController: UICollectionViewDelegate,UICollectionViewDataSource,UICollectionViewDelegateFlowLayout {
+    func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        return 5
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+        let cell = collectionView.dequeueReusableCell(withReuseIdentifier: TvCategoryListViewCellId, for: indexPath)
+        cell.backgroundColor = UIColor.red
+        return cell
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, sizeForItemAt indexPath: IndexPath) -> CGSize {
+        return CGSize(width: (collectionView.frame.width/3) - 10, height: 150)
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, minimumLineSpacingForSectionAt section: Int) -> CGFloat {
+        return 10
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, minimumInteritemSpacingForSectionAt section: Int) -> CGFloat {
+        return 5
+    }
+    
+    
     
     
 }
