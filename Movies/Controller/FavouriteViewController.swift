14a9b9176cf024c1d461fa0dcbc356ec230a83fa
Omar Thamri <omarthamri4@gmail.com>
2019-11-15 21:30:50 +0100

    #0012 implement Favorite View Controller

--- Movies/Controller/FavouriteViewController.swift
+++ Movies/Controller/FavouriteViewController.swift
@@ -30,6 +30,46 @@ class FavouriteViewController: UIViewController {
         return ndv
     }()
     
+    let choiceView: UIView = {
+        let cv = UIView()
+        cv.backgroundColor = UIColor.init(white: 0.25, alpha: 1)
+        cv.translatesAutoresizingMaskIntoConstraints = false
+        return cv
+    }()
+    
+    let moviesBtn: UIButton = {
+        let mb = UIButton()
+        mb.translatesAutoresizingMaskIntoConstraints = false
+        mb.setTitle("Movies", for: .normal)
+        mb.setTitleColor(UIColor.white, for: .normal)
+        mb.titleLabel?.textAlignment = .center
+        return mb
+    }()
+    
+    let tvSeriesBtn: UIButton = {
+        let mb = UIButton()
+        mb.translatesAutoresizingMaskIntoConstraints = false
+        mb.setTitle("TV Serie", for: .normal)
+        mb.setTitleColor(UIColor.white, for: .normal)
+        mb.titleLabel?.textAlignment = .center
+        return mb
+    }()
+    
+    let whiteLine: UIView = {
+       let wl = UIView()
+        wl.backgroundColor = UIColor.white
+        wl.translatesAutoresizingMaskIntoConstraints = false
+        return wl
+    }()
+    
+    let TvSerieWhiteLine: UIView = {
+        let wl = UIView()
+        wl.backgroundColor = UIColor.white
+        wl.translatesAutoresizingMaskIntoConstraints = false
+        wl.isHidden = true
+        return wl
+    }()
+    
     var widthNavDrawer: CGFloat?
     var widthCloseNavDrawer: CGFloat?
     
@@ -48,9 +88,22 @@ class FavouriteViewController: UIViewController {
         view.backgroundColor = UIColor.init(white: 0.2, alpha: 1)
         currentWindow?.addSubview(navDrawerView)
         currentWindow?.addSubview(closeDrawerView)
+        view.addSubview(choiceView)
+        choiceView.addSubview(moviesBtn)
+        choiceView.addSubview(whiteLine)
+        choiceView.addSubview(tvSeriesBtn)
+        choiceView.addSubview(TvSerieWhiteLine)
     }
     
     func setupConstraints() {
+        view.addConstraints(NSLayoutConstraint.constraints(withVisualFormat: "H:|[v0]|", options: NSLayoutFormatOptions(), metrics: nil, views: ["v0":choiceView]))
+        view.addConstraints(NSLayoutConstraint.constraints(withVisualFormat: "V:|-64-[v0(75)]", options: NSLayoutFormatOptions(), metrics: nil, views: ["v0":choiceView]))
+        choiceView.addConstraints(NSLayoutConstraint.constraints(withVisualFormat: "H:|[v0(\(view.frame.width/2))]", options: NSLayoutFormatOptions(), metrics: nil, views: ["v0":moviesBtn]))
+        choiceView.addConstraints(NSLayoutConstraint.constraints(withVisualFormat: "V:|[v0]-1-[v1(1)]|", options: NSLayoutFormatOptions(), metrics: nil, views: ["v0":moviesBtn,"v1":whiteLine]))
+        choiceView.addConstraints(NSLayoutConstraint.constraints(withVisualFormat: "H:|[v0(\(view.frame.width/2))]", options: NSLayoutFormatOptions(), metrics: nil, views: ["v0":whiteLine]))
+        choiceView.addConstraints(NSLayoutConstraint.constraints(withVisualFormat: "H:[v0(\(view.frame.width/2))]|", options: NSLayoutFormatOptions(), metrics: nil, views: ["v0":tvSeriesBtn]))
+        choiceView.addConstraints(NSLayoutConstraint.constraints(withVisualFormat: "V:|[v0]-1-[v1(1)]|", options: NSLayoutFormatOptions(), metrics: nil, views: ["v0":tvSeriesBtn,"v1":TvSerieWhiteLine]))
+        choiceView.addConstraints(NSLayoutConstraint.constraints(withVisualFormat: "H:[v0(\(view.frame.width/2))]|", options: NSLayoutFormatOptions(), metrics: nil, views: ["v0":TvSerieWhiteLine]))
         widthNavDrawer = (currentWindow?.frame.width)! * 2 / 3
         widthCloseNavDrawer = (currentWindow?.frame.width)! / 3
         navDrawerView.widthAnchor.constraint(equalToConstant: widthNavDrawer!).isActive = true
